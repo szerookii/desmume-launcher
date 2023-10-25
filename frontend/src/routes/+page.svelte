b2857a64f440a33300d1632f6ff52c1452ccd237
emberantz <36236750+szerookii@users.noreply.github.com>
2023-10-25 16:28:20 +0200

    Added game auto reload each 3s and a search bar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,11 +2,28 @@
   import { ListGames, LaunchGame } from "$lib/wailsjs/go/main/App.js";
   import { onMount } from "svelte";
   import toast from "svelte-french-toast";
+  import type {FormEventHandler} from "svelte/elements";
 
   let games: any = [];
 
+  $: filtered = games;
+
+  function searchBarFilter(event: FormEventHandler<HTMLInputElement>) {
+      const value = event.target.value.toLowerCase();
+      filtered = games.filter((game: any) => game.Name.toLowerCase().includes(value));
+  }
+
   onMount(async () => {
     games = await ListGames();
+
+    toast.success("Games loaded", {
+        position: "top-right",
+        style: 'border-radius: 200px; background: #333; color: #fff;'
+    });
+
+    setInterval(async () => {
+        games = await ListGames();
+    }, 3000);
   });
 
   async function onDoubleClick(game: any) {
@@ -26,6 +43,8 @@
 
 <section class="min-h-screen scroll-smooth">
   <div class="overflow-x-auto">
+    <input type="text" placeholder="Pokémon White" class="input input-bordered w-full max-w-xs p-2 m-4" on:input={searchBarFilter} />
+
     <table class="table">
       <thead>
         <tr>
@@ -36,7 +55,7 @@
         </tr>
       </thead> 
       <tbody>
-        {#each games as game}
+        {#each filtered as game}
         <tr on:dblclick={(() => onDoubleClick(game))}>
           <th><div class="avatar">
             <div class="w-16 h-16">
